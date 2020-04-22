923688482af8ac9b9c74c6159d7d0ce40a00b9cc
Solibr <33227099+Solibr@users.noreply.github.com>
2020-04-23 00:07:12 +0500

    Double added

--- src/Loader.java
+++ src/Loader.java
@@ -2,6 +2,8 @@ public class Loader{
     public static void main(String[] args) {
         System.out.println("Arrrividerchi");
         System.out.println("Second string");
+        
+        double o = 2.5;
     }
 
 }
